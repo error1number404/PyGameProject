ddb0fcdd9295ed1dca93a236cbe9841327a71401
error1number404 <error1number404@gmail.com>
2021-02-23 18:44:12 +0700

    Delete data directory

--- data/map/minimap/tsx/palette.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="palette" tilewidth="1" tileheight="1" tilecount="64" columns="64">
- <image source="../tiles/the-perfect-palette-20-1x.png" width="64" height="1"/>
-</tileset>
